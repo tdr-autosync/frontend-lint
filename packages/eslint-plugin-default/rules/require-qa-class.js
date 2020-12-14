bd7f215fe64da2895ed5fec3fbf3fe110cdb6004
Serhii Petkun <sergeypetkun@gmail.com>
2020-12-14 13:50:56 +0200

    FE-84: implement "require-qa-class" ESLint rule

---
+++ packages/eslint-plugin-default/rules/require-qa-class.js
@@ -0,0 +1,110 @@
+const utils = require('eslint-plugin-vue/lib/utils');
+
+function checkLiteral(value) {
+  return /(?:^|\s)qa-[a-z0-9_-]+/.test(value);
+}
+
+function checkTemplateExpression(expression) {
+  return expression.quasis.some(element =>
+    /(?:^|\s)qa-(?:$|[a-z0-9_-]+)/.test(element.value.cooked),
+  );
+}
+
+function checkArrayExpression(expression) {
+  return expression.elements.some(
+    element =>
+      (element.type === 'Literal' && checkLiteral(element.value)) ||
+      (element.type === 'TemplateLiteral' && checkTemplateExpression(element)),
+  );
+}
+
+function checkObjectExpression(expression) {
+  return expression.properties.some(
+    property =>
+      (property.key.type === 'Literal' && checkLiteral(property.key.value)) ||
+      (property.key.type === 'TemplateLiteral' && checkTemplateExpression(property.key)),
+  );
+}
+
+function checkExpression(expression) {
+  if (expression.type === 'ArrayExpression') {
+    return checkArrayExpression(expression);
+  } else if (expression.type === 'ObjectExpression') {
+    return checkObjectExpression(expression);
+  } else {
+    return false;
+  }
+}
+
+function reportNoQaClass(context, node) {
+  context.report({
+    node,
+    loc: node.loc,
+    message: 'qa-* CSS class is required on this element',
+  });
+}
+
+module.exports = {
+  meta: {
+    type: 'problem',
+    docs: {
+      description: 'require `qa-*` class on provided elements',
+    },
+    fixable: null,
+    schema: [{
+      type: 'object',
+      properties: {
+        elements: {
+          type: 'array',
+          items: {
+            type: 'string',
+          },
+        },
+        additionalElements: {
+          type: 'array',
+          items: {
+            type: 'string',
+          },
+        },
+      },
+    }],
+  },
+  create(context) {
+    const options = {
+      elements: ['button', 'input', 'select', 'textarea'],
+      additionalElements: [],
+      ...(context.options[0] || {}),
+    };
+    const elementsToCheck = [...options.elements, ...options.additionalElements].map(item =>
+      item.toLowerCase(),
+    );
+
+    return utils.defineTemplateBodyVisitor(context, {
+      VElement(node) {
+        if (!elementsToCheck.includes(node.name.toLowerCase())) {
+          return;
+        }
+
+        const classAttribute = utils.getAttribute(node, 'class');
+        if (
+          classAttribute &&
+          classAttribute.value &&
+          classAttribute.value.type === 'VLiteral' &&
+          checkLiteral(classAttribute.value.value)
+        ) {
+          return;
+        }
+
+        const classDirective = utils.getDirective(node, 'bind', 'class');
+        if (
+          classDirective &&
+          classDirective.value.type === 'VExpressionContainer' &&
+          checkExpression(classDirective.value.expression)
+        ) {
+          return;
+        }
+        reportNoQaClass(context, node);
+      },
+    });
+  },
+};
---
+++ packages/eslint-plugin-default/test/rules/require-qa-class.spec.js
@@ -0,0 +1,74 @@
+const rule = require('../../rules/require-qa-class');
+const RuleTester = require('eslint').RuleTester;
+
+const errorMessage = 'qa-* CSS class is required on this element';
+
+const tester = new RuleTester({
+  parser: require.resolve('vue-eslint-parser'),
+  parserOptions: {
+    ecmaVersion: 2015,
+  },
+});
+
+tester.run('require-qa-class', rule, {
+  valid: [
+    '<template><div></div></template>',
+    '<template><input class="qa-btn" type="text" /></template>',
+    '<template><button class="qa-btn"></button></template>',
+    '<template><button :class="[\'qa-btn\']"></button></template>',
+    '<template><button :class="[`qa-${value}`]"></button></template>',
+    '<template><button :class="{ \'qa-btn\': value }"></button></template>',
+    '<template><button :class="{ [`qa-${value}`]: value }"></button></template>',
+    {
+      code: '<template><button></button></template>',
+      options: [
+        {
+          elements: ['input'],
+        },
+      ],
+    },
+  ],
+  invalid: [
+    {
+      code: '<template><input type="text" /></template>',
+      errors: [errorMessage],
+    },
+    {
+      code: '<template><input class="Input" type="text" /></template>',
+      errors: [errorMessage],
+    },
+    {
+      code: '<template><button></button></template>',
+      errors: [errorMessage],
+    },
+    {
+      code: '<template><button class="Btn"></button></template>',
+      errors: [errorMessage],
+    },
+    {
+      code: '<template><button :class="[\'Btn\']"></button></template>',
+      errors: [errorMessage],
+    },
+    {
+      code: '<template><button :class="[`Btn-${value}`]"></button></template>',
+      errors: [errorMessage],
+    },
+    {
+      code: '<template><button :class="{ \'Btn-btn\': value }"></button></template>',
+      errors: [errorMessage],
+    },
+    {
+      code: '<template><button :class="{ [`Btn-${value}`]: value }"></button></template>',
+      errors: [errorMessage],
+    },
+    {
+      code: '<template><NButton></NButton></template>',
+      options: [
+        {
+          additionalElements: ['NButton'],
+        },
+      ],
+      errors: [errorMessage],
+    },
+  ],
+});
